f2cc444c775d07846b63b6407a567d1719fcb71b
Harrison Bronfeld <27023177+hashbron@users.noreply.github.com>
2023-11-29 00:31:42 -0500

    Adding button for hover effects.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function checkboxClicked() {
 	var captcha = document.getElementById('captcha');
+	var checkbox = document.getElementById('checkbox');
+	checkbox.style.visibility = "hidden";
 	captcha.src = "open.gif";
 }
