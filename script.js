a4d002bf25e66a7e012b889ed4e1a0c05aff1101
Taylor Paydos <taylorpaydos@Taylors-MacBook-Pro.local>
2023-11-29 20:17:20 -0500

    css

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function openGif() {
 	var captcha = document.getElementById('captcha');
 	captcha.src = "open.gif";
 }
+
+function about() {
+	var x = document.getElementById("captchaOne");
+	var a = document.getElementById("captchaTwo");
+
+	if (a.style.display === "none") {
+	  x.style.display = "block";
+	  a.style.display = "block";
+	} else {
+	  a.style.display = "none";
+	}
+  }
+
+ 
