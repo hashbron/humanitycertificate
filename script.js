facbd5e231e0c0734cd1577266942d3371a66ea8
Taylor Paydos <taylorpaydos@Taylors-MacBook-Pro.local>
2023-11-29 23:53:49 -0500

    organizing image files

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkboxClicked() {
 	var captcha = document.getElementById('captcha');
 	var checkbox = document.getElementById('checkbox');
 	checkbox.style.visibility = "hidden";
-	captcha.src = "open.gif";
+	captcha.src = "assets/initialCaptcha/open.gif";
 }
 
  function captchaSevenClicked(){
